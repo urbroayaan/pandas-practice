572c405d94195c193118c08b38d2a9a7d75c739b
Ayaan Anildutt <144311315+urbroayaan@users.noreply.github.com>
2025-12-16 13:49:11 +0530

    Filter rows using conditions

--- analysis.py
+++ analysis.py
@@ -8,7 +8,13 @@ df = pd.read_csv("employees.csv")
 # df.info() - describes all table header info with datatypes, memory allocation per head and total memory usage
 # df.describe() - provides statistical analysis of the table data
 
-names = df["name"]
-subset = df[["name", "department", "salary"]]
-print(names.head())
-print(subset.head())
+# names = df["name"] - selecting singular column
+# subset = df[["name", "department", "salary"]] - selecting multiple columns by passing list
+
+high_salary = df[df["salary"] > 90000]
+it_emp = df[df["department"] == "IT"]
+it_high_paid = df[(df["department"] == "IT") & (df["salary"] > 90000)]
+
+print(high_salary,"\n")
+print(it_high_paid, "\n")
+print(it_emp, "\n")
