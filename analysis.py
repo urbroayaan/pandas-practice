d67be1c8bb1fc84aee56d7ae751ca3d2d82a2a1e
Ayaan Anildutt <144311315+urbroayaan@users.noreply.github.com>
2025-12-16 13:43:11 +0530

    Select single and multiple columns

--- analysis.py
+++ analysis.py
@@ -5,5 +5,10 @@ df = pd.read_csv("employees.csv")
 # df.head() - returns first 5 rows
 # df.shape -  returns (rows, columns)
 
-print(df.info())
-print(df.describe())
+# df.info() - describes all table header info with datatypes, memory allocation per head and total memory usage
+# df.describe() - provides statistical analysis of the table data
+
+names = df["name"]
+subset = df[["name", "department", "salary"]]
+print(names.head())
+print(subset.head())
